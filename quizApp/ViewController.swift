d58d335c6f6b9f4e2e7be16f1903c979b9a495c1
Mustafa <wadupbiatch@icloud.com>
2021-12-27 17:12:48 +0300

    Initial Commit

---
+++ quizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quizApp
+//
+//  Created by Mustafa on 27.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
