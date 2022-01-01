748a106bc04b4e96375d3c3e7836c11339a548e2
Mustafa <wadupbiatch@icloud.com>
2022-01-02 00:57:40 +0300

    first commit

---
+++ quizApp/Model/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  quizApp
+//
+//  Created by Mustafa on 27.12.2021.
+//
+
+import Foundation
---
+++ quizApp/Model/QuizBrain.swift
@@ -0,0 +1,8 @@
+//
+//  QuizBrain.swift
+//  quizApp
+//
+//  Created by Mustafa on 27.12.2021.
+//
+
+import Foundation
